d9af0ea42aad81db477e941bae394520e7565d60
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-01-30 19:57:54 +0900

    change:migration_file

--- db/migrate/20250128105647_create_records.rb
+++ db/migrate/20250128105647_create_records.rb
@@ -1,6 +1,12 @@
 class CreateRecords < ActiveRecord::Migration[7.0]
   def change
     create_table :records do |t|
+      t.decimal :study_time, null: false
+      t.string :title, null: false
+      t.date :date, null: false
+      t.text :details, null: false
+      t.references :user_id, null: false, foreign_key: true
+      t.references :goal_id, null: false, foreign_key: true
 
       t.timestamps
     end
