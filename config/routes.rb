8107212d8d1139cb3dd6c4ca534d2e92c153053f
lake torrens <laketorrens@lakenoMacBook-Air.local>
2025-02-03 23:16:38 +0900

    Change:20250203_partial-files

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "static_pages#top"
 
-  resouces :user_settings, only: %i[edit update]
-  resouces :study_goals, only: %i[new create edit update]
-  resouces :password_updates, only: %i[edit update]
-  resouces :study_records, only: %i[new create confirm commit]
-  resouces :mypages, only: %i[show edit_goal update_goal]
-  resouces :user_registrations, only: %i[new create]
+  resources :user_settings, only: %i[edit update]
+  resources :study_goals, only: %i[new create edit update]
+  resources :password_updates, only: %i[edit update]
+  resources :study_records, only: %i[new create confirm commit]
+  resources :mypages, only: %i[show edit_goal update_goal]
+  resources :user_registrations, only: %i[new create]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
